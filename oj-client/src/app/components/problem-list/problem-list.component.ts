75aa8d07204495b6d791368d9b181920b882391e
Yi Wei <usc_weiyi@foxmail.com>
2018-02-23 09:59:55 -0800

    add components

---
+++ oj-client/src/app/components/problem-list/problem-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-problem-list',
+  templateUrl: './problem-list.component.html',
+  styleUrls: ['./problem-list.component.css']
+})
+export class ProblemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
